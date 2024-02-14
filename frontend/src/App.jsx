4763c17ddeac6d3d900cb63ee6a30b23ebb00f4d
Agenor2512 <noemie.vasseur2512@gmail.com>
2024-02-14 14:44:03 +0100

    Changes on the outlet component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,7 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return (
-    <main>
-      <Outlet />
-    </main>
-  );
+  return <Outlet />;
 }
 
 export default App;
