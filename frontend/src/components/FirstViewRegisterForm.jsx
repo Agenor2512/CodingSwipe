23a696040755f4ca7b5e137d91d7b70e23b98fb0
Agenor2512 <noemie.vasseur2512@gmail.com>
2024-02-15 16:17:26 +0100

    Added the first view form with content

--- frontend/src/components/FirstViewRegisterForm.jsx
+++ frontend/src/components/FirstViewRegisterForm.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function FirstViewRegisterForm() {
-  return <div>FirstViewRegisterForm</div>;
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <h1>Créer un compte</h1>
+      <h3>Sélectionnez votre statut</h3>
+      <button type="button">Je suis une entreprise</button>
+      <button type="button">Je recherche un emploi</button>
+      <input type="submit" value="Continuer" />
+    </form>
+  );
 }
 
 export default FirstViewRegisterForm;
