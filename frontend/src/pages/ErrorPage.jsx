2e0dda5f271d0cff4bcb7b5c6dd73d003f2a8f07
Lea732 <leapoulin07@gmail.com>
2024-02-15 14:59:58 +0100

    Centered error_page_container

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 import notFoundIcon from "../assets/404_error.png";
 import homeIcon from "../assets/home_icon.png";
 
+import "../styles/errorPage.css";
+
 function ErrorPage() {
   return (
-    <>
+    <div className="error_page_container">
       <img
         src={notFoundIcon}
         alt="Computer icon with a 404 error on the screen"
@@ -19,7 +21,7 @@ function ErrorPage() {
           <p>Retour à l'accueil</p>
         </Link>
       </div>
-    </>
+    </div>
   );
 }
 
