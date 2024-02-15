237e4cf5bb0a3fbab829238630127c048583fab5
Lea732 <leapoulin07@gmail.com>
2024-02-15 12:08:56 +0100

    Added new assets to frontend/assets folder, added content to errorPage

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import notFoundIcon from "../assets/404_error.png";
+import homeIcon from "../assets/home_icon.png";
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <img
+        src={notFoundIcon}
+        alt="Computer icon with a 404 error on the screen"
+      />
+      <h1>404 NotFound</h1>
+      <p>Malheureusement, l'élément auquel vous voulez accéder n'existe pas.</p>
+      <div>
+        <Link to="/">
+          <img src={homeIcon} alt="Home icon" />
+          <p>Retour à l'accueil</p>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default ErrorPage;
