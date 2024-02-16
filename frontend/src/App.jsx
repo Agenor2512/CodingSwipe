1e88968209d7dfc9075e39f5459a5391697b2213
Mansour51 <mansour.zakreev@hotmail.com>
2024-02-16 15:26:01 +0100

    Add Title for the Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
+import TitlesDescription from "./components/TitlesDescription";
 import "./App.css";
 
 function App() {
-  return <Outlet />;
+  return (
+    <div>
+      <Outlet />
+      <TitlesDescription />
+    </div>
+  );
 }
 
 export default App;
