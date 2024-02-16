e6c72cd10fbb147a52eeef4f07c7bcc68042ec44
Agenor2512 <noemie.vasseur2512@gmail.com>
2024-02-16 22:49:52 +0100

    Added form step two for enterprise account creation with minimum CSS

--- frontend/src/components/register/StepTwo.jsx
+++ frontend/src/components/register/StepTwo.jsx
@@ -1,7 +1,98 @@
 import React from "react";
 
+import "../../styles/stepTwo.css";
+
+/* TODO : 
+    - Trouver une solution pour les accolades dans "Mes langages informatiques" 
+*/
+
 function StepTwo() {
-  return <div>StepTwo</div>;
+  const mainCourses = ["Frontend", "Backend", "Full Stack"];
+
+  const candidateLevels = ["Junior", "Mid-level", "Senior"];
+
+  const enterpriseExpectations = [
+    "Un CDI",
+    "Un CDD",
+    "Un Stage / Une Alternance",
+    "Du freelance",
+  ];
+
+  const enterpriseWorkplaces = ["Sur site", "Remote partiel", "Full remote"];
+
+  const computerLanguages = [
+    "HTML/CSS",
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby On Rails",
+    "Vue.js",
+    "Swift",
+    "Kotlin",
+    "Flutter",
+    "Go",
+    "C#",
+    "C++",
+    "React",
+    ".NET Core / .NET 5",
+    "SQL",
+    "NoSQL",
+  ];
+
+  return (
+    <form action="submit" className="step_two_form">
+      <h1>Créer Un Compte</h1>
+      <p>
+        <span>"square" </span>ÉTAPE 2<span>-</span>Répondez à ces quelques
+        questions
+      </p>
+      <p>
+        Appétences <span>:</span>
+      </p>
+      {mainCourses.map((mainCourse) => (
+        <button type="button">{mainCourse}</button>
+      ))}
+
+      <p>
+        Je recherche <span>:</span>
+      </p>
+      {enterpriseExpectations.map((enterpriseExpectation) => (
+        <div>
+          <input type="checkbox" id="checkbox" />
+          <label htmlFor="checkbox">{enterpriseExpectation}</label>
+        </div>
+      ))}
+
+      <p>
+        Lieu de travail <span>:</span>
+      </p>
+      {enterpriseWorkplaces.map((enterpriseWorkplace) => (
+        <div>
+          <input type="checkbox" id="checkbox" />
+          <label htmlFor="checkbox">{enterpriseWorkplace}</label>
+        </div>
+      ))}
+
+      <p>
+        Je suis <span>:</span>
+      </p>
+      {candidateLevels.map((candidateLevel) => (
+        <button type="button">{candidateLevel}</button>
+      ))}
+      <p>
+        Mes langages informatiques
+        <span>sélectionnez au moins un langage</span>
+      </p>
+      {computerLanguages.map((computerLanguage) => (
+        <div>
+          <input type="checkbox" id="checkbox" />
+          <label htmlFor="checkbox">{computerLanguage}</label>
+        </div>
+      ))}
+
+      <input type="submit" value="Finaliser l'inscription" />
+    </form>
+  );
 }
 
 export default StepTwo;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import App from "./App";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import UsersHomePage from "./pages/UsersHomePage";
+import StepTwo from "./components/register/StepTwo";
 import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: "/usersHomePage",
         element: <UsersHomePage />,
+      },
+      {
+        path: "/stepTwo",
+        element: <StepTwo />,
       },
     ],
     errorElement: <ErrorPage />,
