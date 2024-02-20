4d843ce48aa4b771285b89284d98a35c423d10ad
farah <farahchikhoune1@gmail.com>
2024-02-20 16:41:24 +0100

    Connection button and front end modal done with email and password input

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
 import Logo from "../components/Logo";
+import ModalConnection from "../components/ModalConnection";
 
 function HomePage() {
-  return <Logo />;
+  return (
+    <>
+      <Logo />
+      <ModalConnection />
+    </>
+  );
 }
 
 export default HomePage;
