e46676296d5705089de6bd4e78ec6d1b0376ebe9
Mansour51 <mansour.zakreev@hotmail.com>
2024-02-21 16:35:01 +0100

    feat/US10_thirdStepAccountCreation

--- frontend/src/components/ThirdStepAccountCreation.jsx
+++ frontend/src/components/ThirdStepAccountCreation.jsx
@@ -1,35 +1,19 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
 import ModalConnection from "./ModalConnection";
 
 import "../styles/thirdStepAccountCreation.css";
 
 function AccountCreation() {
-  const [setModal] = useState(false);
-
-  const toggleModalOnce = () => {
-    setModal(true);
-  };
-
   return (
-    <div>
-      <h3> Crée Un Compte</h3>
-      <div className="connect">
-        <div className="square"> </div>
+    <div className="final_step_container">
+      <h3> Créer Un Compte</h3>
+      <div className="subtitle">
+        <div className="square" />
         <p>
-          INSCRIPTION TERMINÉE <span>-</span> Rendez vous sur l’espace{" "}
+          INSCRIPTION TERMINÉE &nbsp; <span>-</span>&nbsp; Rendez vous sur
+          l’espace &nbsp;
           <span>connexion</span>.
         </p>
       </div>
-      <Link to="/modal-connection">
-        <button
-          type="button"
-          className="connection_button"
-          onClick={toggleModalOnce}
-        >
-          Me connecter
-        </button>
-      </Link>
       <ModalConnection />
     </div>
   );
