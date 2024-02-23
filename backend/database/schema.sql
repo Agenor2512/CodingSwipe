f5ea96c6ee33eccfe4b0f9094ecb52d542290aab
Agenor2512 <noemie.vasseur2512@gmail.com>
2024-02-23 14:36:01 +0100

    Added basic script for bdd and testing API routes

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,15 @@
-create table item (
+create table candidate (
   id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+  lastname varchar(100) not null,
+  firstname varchar(100) not null,
+  email varchar(100) not null,
+  department varchar(100) not null,
+  _password varchar(100) not null,
+  appetence  varchar(100) not null,
+  job_type  varchar(100) not null,
+  work_rhythm  varchar(100) not null,
+  experience_level  varchar(100) not null,
+  programming_languages  varchar(100) not null
 );
+
+insert into candidate values()
