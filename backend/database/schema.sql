0b79635a8e08debe8dcf2ce400fc0a1398307d13
Mathieu Sinoir <99977739+MathieuSinoir@users.noreply.github.com>
2024-03-04 16:25:17 +0100

    modified SQL file

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,15 +1,17 @@
-CREATE TABLE company(
+CREATE TABLE enterprise (
   id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id),
   name VARCHAR(100) NOT NULL,
   siret INT NOT NULL,
   legal_status VARCHAR(100) NOT NULL,
   business_sector VARCHAR(100) NOT NULL,
-  description TEXT NOT NULL
+  description TEXT NOT NULL,
+  email VARCHAR(320) NOT NULL,
+  password VARCHAR(100) NOT NULL
 );
 
-INSERT INTO company (name, siret, legal_status, business_sector, description)
+INSERT INTO enterprise (name, siret, legal_status, business_sector, description, email, password)
 VALUES
-('Carrefour', 123456, 'SAS', 'grande-distribution', 'Acteur mondial de la grande distribution'),
-('Lidl', 214654,'SAS', 'grande-distribution', 'Acteur mondial de la grande distribution'),
-('Google', 657392,'SAS', 'Informatique', 'Vous connaissez pas Google??');
+('Carrefour', 123456, 'SAS', 'grande-distribution', 'Acteur mondial de la grande distribution', "lisa@carrefour.com", "monmdpsecret"),
+('Lidl', 214654,'SAS', 'grande-distribution', 'Acteur mondial de la grande distribution', "yop@lidl.com", "lidlemotdepasse"),
+('Google', 657392,'SAS', 'Informatique', 'Vous connaissez pas Google??', "louis@google.com", "googlechutchut");
