b1bda74cfcf298088eb5319e90f0cd957674ccee
marcopoltergeist <paul_dubor@outlook.fr>
2024-03-05 17:31:36 +0100

    update css selector

--- frontend/src/components/HomePageUserNav.jsx
+++ frontend/src/components/HomePageUserNav.jsx
@@ -2,38 +2,7 @@ import React, { useState } from "react";
 import "../styles/homePageUserNav.css";
 
 function HomePageUserNav() {
-  /* const matchesData = [
-    {
-      id: 1,
-      name: "Paul",
-      category: "Développeur Frontend",
-      date: "2024-02-23",
-      region: "Bordeaux",
-      jobName: "Développeur Web Junior",
-    },
-    {
-      id: 2,
-      name: "Nono",
-      category: "Data Scientist",
-      date: "2024-02-23",
-      region: "Lille",
-      jobName: "Data Analyst",
-    },
-    {
-      id: 3,
-      name: "Mathieu",
-      category: "Chef de Projet",
-      date: "2024-02-23",
-      region: "Marseille",
-      jobName: "Chef de Projet IT",
-    },
-  ]; */
-
   const [isNavOpen, setIsNavOpen] = useState(false);
-  // const [userType, setUserType] = useState();
-  // const [matches, setMatches] = useState([]);
-  // const [messsages, setMessages] = useState("");
-  // const [myProfile, setMyProfile] = useState([]);
 
   const openNav = () => {
     setIsNavOpen(true);
@@ -43,45 +12,9 @@ function HomePageUserNav() {
     setIsNavOpen(false);
   };
 
-  /* const handleTabClick = (type) => {
-    setUserType(type);
-    if (type === "match") {
-      setTimeout(() => {
-        // setMatches(matchesData);
-      }, 1000);
-    }
-  }; */
-
-  // Commented out the renderMatches function
-  /*
-  const renderMatches = () => {
-    return matchesData.map((match) => {
-      return (
-        <div key={match.id} className="match_card">
-          {userType === "candidat" ? (
-            <>
-              <div>Initiale id entreprise ??{match.name}</div>
-              <div>{match.category}</div>
-              <div>Région de l'employeur: {match.region}</div>
-              <div>Intitulé du poste {match.jobName}</div>
-            </>
-          ) : (
-            <>
-              <div>Initial du candidat ?? {match.name}</div>
-              <div>Intitulé de son poste: {match.category}</div>
-              <div>Date: {match.date}</div>
-              <div>Intitulé du poste qu'il a liké {match.jobName}</div>
-            </>
-          )}
-        </div>
-      );
-    });
-  };
-  */
-
   return (
-    <section>
-      <div className="desktopNav">
+    <section className="user_navigation_container">
+      <div className="desktop_nav">
         <ul>
           <li>MATCHS</li>
           <li>MESSAGES</li>
