76bd7e5a4f64dbc4ddf3b4894d15445e28ebb7cb
Mathieu Sinoir <99977739+MathieuSinoir@users.noreply.github.com>
2024-03-11 15:25:52 +0100

    Add input to write text and send button some CSS

--- frontend/src/components/messages/MessagesBox.jsx
+++ frontend/src/components/messages/MessagesBox.jsx
@@ -3,11 +3,21 @@ import "../../styles/message_box/MessageBox.css";
 
 function MessagesBox() {
   return (
-    <section className="check_advert">
-      <div>
-        <button type="button">Voir l'annonce</button>
-      </div>
-    </section>
+    <>
+      <section className="write_text">
+        <div>
+          <input className="texte_area" type="text" id="text" />
+        </div>
+        <div className="send">
+          <button type="button">Envoyer</button>
+        </div>
+      </section>
+      <section className="check_advert">
+        <div>
+          <button type="button">Voir l'annonce</button>
+        </div>
+      </section>
+    </>
   );
 }
 
