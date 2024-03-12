004f0b98beecdf14e5405c66a439400f0ac03be9
Agenor2512 <noemie.vasseur2512@gmail.com>
2024-03-12 19:16:48 +0100

    Added content to swipe components (candidate + enterprise) and applied mobile style to CandidateCandidacy

---
+++ frontend/src/components/content_to_swipe/CandidateCandidacy.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import "../../styles/content_to_swipe/candidateCandidacy.css";
+
+function CandidateResume() {
+  const candidateDescription =
+    "Passionné par le développement et animé par l'innovation, je suis un développeur junior déterminé à apporter ma curiosité technique et ma créativité au sein d'une startup dynamique.";
+
+  return (
+    <div className="candidate_infos_container">
+      <div className="candidate_infos_header">
+        <div>W</div>
+        <section>
+          <h1>Développeur/Développeuse</h1>
+        </section>
+      </div>
+
+      <div className="modify_display_in_desktop">
+        <div>
+          <h2>Qui suis-je ?</h2>
+          <p>{candidateDescription}</p>
+        </div>
+
+        <section className="candidate_expectations_container">
+          <h2>Ma recherche</h2>
+        </section>
+      </div>
+
+      <div className="skills_languages_experiences_desktop">
+        <section className="soft_skills">
+          <h2>Soft skills</h2>
+          <div />
+        </section>
+
+        <section className="computer_language_checkbox_container">
+          <h2>Langages informatiques</h2>
+          <div />
+        </section>
+
+        <section className="significatives_experiences">
+          <h2>Expériences significatives</h2>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default CandidateResume;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,7 @@ import Register from "./pages/Register";
 import UsersHomePage from "./pages/UsersHomePage";
 import ErrorPage from "./pages/ErrorPage";
 import { RegisterProvider } from "./context/RegisterContext";
+import CandidateCandidacy from "./components/content_to_swipe/CandidateCandidacy";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
       {
         path: "/usersHomePage",
         element: <UsersHomePage />,
+      },
+      {
+        path: "/candidateCandidacy",
+        element: <CandidateCandidacy />,
       },
     ],
     errorElement: <ErrorPage />,
