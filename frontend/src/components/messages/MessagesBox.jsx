06328cb83b73dee62c04b60212480a0f42bcfda5
Mathieu Sinoir <99977739+MathieuSinoir@users.noreply.github.com>
2024-03-13 19:21:00 +0100

    CSS done need to put the btn in the text area

--- frontend/src/components/messages/MessagesBox.jsx
+++ frontend/src/components/messages/MessagesBox.jsx
@@ -3,20 +3,14 @@ import "../../styles/message_box/MessageBox.css";
 
 function MessagesBox() {
   return (
-    <section className="text_with_each_other">
-      <section className="write_text">
-        <div>
-          <input className="texte_area" type="text" id="text" />
-        </div>
-        <div className="send">
-          <button type="button">Envoyer</button>
-        </div>
-      </section>
-      <section className="check_advert">
-        <div>
-          <button type="button">Voir l'annonce</button>
-        </div>
-      </section>
+    <section className="swipe_system_container">
+      <div className="send_text_Button">
+        <input className="texte_area" type="text" id="text" />
+      </div>
+
+      <div className="button_see_advert">
+        <button type="button">Voir l'annonce</button>
+      </div>
     </section>
   );
 }
