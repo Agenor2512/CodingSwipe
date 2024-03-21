0c04e6b1c1203b5100c995b8ba512cd1ef29a7a6
Lea732 <leapoulin07@gmail.com>
2024-03-21 16:32:28 +0100

    Added media querie to message box

--- frontend/src/pages/HomePageMessages.jsx
+++ frontend/src/pages/HomePageMessages.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import MessagesBox from "../components/resume_job_offer/MessagesBox";
 
+import "../styles/pages/homePageMessages.css";
+
 function HomePageMessages() {
   return (
     <div className="homepage_messages">
