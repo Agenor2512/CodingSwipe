f31c4bb03ffb0d551843178d12243be7bda401ae
Lea732 <leapoulin07@gmail.com>
2024-03-22 11:07:22 +0100

    Added enterprise validator with Joi

---
+++ backend/src/middlewares/enterpriseValidator.js
@@ -0,0 +1,51 @@
+const Joi = require("joi");
+
+const validateEnterprise = () => {
+  const enterpriseSchema = Joi.object({
+    id: Joi.string()
+      .guid({ version: "uuidv4" })
+      .messages({
+        "string.guid": "Erreur lors de l'ajout de l'id",
+      })
+      .required(),
+
+    name: Joi.string()
+      .pattern(/^[a-zA-Z0-9\s]+$/)
+      .min(3)
+      .max(50)
+      .messages({
+        "string.min": "Votre nom doit au moins contenir 3 caractères.",
+        "string.max": "Votre nom doit faire au maximum 50 caractères.",
+        "string.empty": "Veuillez remplir le champ : Nom de l'entreprise.",
+      })
+      .required(),
+
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "fr"] } })
+      .pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$/)
+      .required(),
+
+    password: Joi.string().pattern(/^(?=.*[*#])[a-zA-Z0-9*#]{8,15}$/),
+    repeat_password: Joi.ref("password"),
+  });
+
+  const enterpriseData = {
+    id: "2de1feec-a19a-4f16-9226-af782acdab42",
+    name: "Java Corp",
+    email: "javacorp@gmail.com",
+    password: "codingsw*ipe25*",
+    repeat_password: "codingsw*ipe25*",
+  };
+
+  const { error, value } = enterpriseSchema.validate(enterpriseData);
+
+  if (error) {
+    console.error("Validation failed:", error.message);
+  } else {
+    console.info("Validation succeeded:", value);
+  }
+};
+
+validateEnterprise();
+
+module.exports = { validateEnterprise };
