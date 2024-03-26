1089b1556813798d3a576b8a55b29084d3e17c99
marcopoltergeist <paul_dubor@outlook.fr>
2024-03-26 16:15:26 +0100

    add a usestate to like and dislike

--- frontend/src/components/content_to_swipe/SwipeSystem.jsx
+++ frontend/src/components/content_to_swipe/SwipeSystem.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import { useState } from "react";
 import "../../styles/content_to_swipe/swipeSystem.css";
 
 function SwipeSystem() {
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+
+  const handleLikeClick = () => {
+    setLikes(likes + 1);
+  };
+
+  const handleDislikeClick = () => {
+    setDislikes(dislikes + 1);
+  };
+
   return (
     <section className="swipe_system_container">
       <div>
-        <button type="button">
+        <button type="button" onClick={handleLikeClick}>
           <img
             className="like_icon"
             src="/src/assets/heart_swipe.svg"
-            alt="green hart"
+            alt="green heart"
           />
         </button>
+        <span>{likes}</span>
 
-        <button type="button">
+        <button type="button" onClick={handleDislikeClick}>
           <img
             className="dislike_icon"
             src="/src/assets/cross_swipe.svg"
             alt="red cross"
           />
         </button>
+        <span>{dislikes}</span>
       </div>
     </section>
   );
