136e31a832c9f710e1a4056fd2ac5de6509e9611
Agenor2512 <noemie.vasseur2512@gmail.com>
2024-04-09 12:00:33 +0200

    Added services files in front

---
+++ frontend/src/services/jobOfferService.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+const baseURL = import.meta.env.VITE_BACKEND_URL;
+
+const client = axios.create({
+  baseURL,
+  timeout: 60_000,
+});
+
+const readById = (id) => {
+  return client
+    .get(`/joboffer/${id}`)
+    .then((response) => response.data)
+    .catch((error) => console.error(error));
+};
+
+module.exports = { readById };
---
+++ frontend/src/services/workRhythmsService.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+const baseURL = import.meta.env.VITE_BACKEND_URL;
+
+const client = axios.create({
+  baseURL,
+  timeout: 60_000,
+});
+
+const readAll = () => {
+  return client
+    .get("/workrhythms")
+    .then((response) => response.data)
+    .catch((error) => console.error(error));
+};
+
+module.exports = { readAll };
